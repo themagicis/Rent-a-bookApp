ec19e14e1c323cfb666c843a9a07284b93dbd775
themagicis <themagicis777@gmail.com>
2014-11-09 08:17:28 +0200

    Added Automapper + Ninject

---
+++ RentABook.Web.Infrastructure/Mapping/IMapFrom.cs
@@ -0,0 +1,6 @@
+﻿namespace RentABook.Web.Infrastructure.Mapping
+{
+    public interface IMapFrom<T>
+    {
+    }
+}
--- RentABook.Web/Global.asax.cs
+++ RentABook.Web/Global.asax.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace RentABook.Web
+﻿namespace RentABook.Web
 {
+    using System.Reflection;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    using RentABook.Web.Infrastructure.Mapping;
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
@@ -16,6 +15,9 @@ namespace RentABook.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var autoMapperConfig = new AutoMapperConfig(Assembly.GetExecutingAssembly());
+            autoMapperConfig.Execute();
         }
     }
 }
